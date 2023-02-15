0226d519613125d6c7cee49b0e88ab86ca7682a9
ChloeD <chloe.desflaches@gmail.com>
2023-02-15 17:08:29 +0100

    création des objets jeu et de la console

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,10 +1,21 @@
 package test;
 
+import model.*;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Console c1 = new Console("Nintendo DS");
+		
+		Jeu jeu1 = new Jeu("New Super Mario Bros", c1);
+		Jeu jeu2 = new Jeu("The Legend Of Zelda", c1);
+		Jeu jeu3 = new Jeu("Animal Crossing : Wild World", c1);
+		Jeu jeu4 = new Jeu("Pokemon Rubis", c1);
+		Jeu jeu5 = new Jeu("Sims City", c1);
+
+
+		
 	}
 
 }
