c6ab2766cd8458337a1d64f93795fdd1e57a71b7
julienkot <julienkot.c@gmail.com>
2023-02-15 17:37:07 +0100

    Ajout des 2 clients avec une liste d'achat

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.*;
 
 public class Test {
@@ -13,6 +16,20 @@ public class Test {
 		Jeu jeu3 = new Jeu("Animal Crossing : Wild World", c1);
 		Jeu jeu4 = new Jeu("Pokemon Rubis", c1);
 		Jeu jeu5 = new Jeu("Sims City", c1);
+		
+		//Ajout de 2 clients
+		//Faire une liste d'achat Client 1
+		List<Jeu> lAchatCl1 = new ArrayList();
+		lAchatCl1.add(jeu1);
+		lAchatCl1.add(jeu3);
+		lAchatCl1.add(jeu4);
+		//Faire une liste d'achat Client 2
+		List<Jeu> lAchatCl2 = new ArrayList();
+		lAchatCl2.add(jeu2);
+		lAchatCl2.add(jeu5);
+		
+		Client cl1 = new Client("Al","Colik",lAchatCl1);
+		Client cl2 = new Client("Laure","Dure",lAchatCl2);
 
 
 		
