fe3f7dd8a81134d0340b37f91692365728a6b7ea
Lina Allel <lina.allel@live.fr>
2023-02-15 19:12:27 +0100

    Console+Test
    Modif Console:  attribut prix et dateSortie ajouté, -modif constructeur
    2 objets dans test : Adresse a1 + Boutique b1)

--- NintendoProject/src/main/java/model/Console.java
+++ NintendoProject/src/main/java/model/Console.java
@@ -3,9 +3,15 @@ package model;
 public class Console {
 
 	private String nom;
+	private int prix;
+	private String dateDeSortie;
 
-	public Console(String nom) {
+
+	public Console(String nom, int prix, String dateDeSortie) {
+		super();
 		this.nom = nom;
+		this.prix = prix;
+		this.dateDeSortie = dateDeSortie;
 	}
 
 	public String getNom() {
@@ -15,10 +21,28 @@ public class Console {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+	
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public String getDateDeSortie() {
+		return dateDeSortie;
+	}
+
+	public void setDateDeSortie(String dateDeSortie) {
+		this.dateDeSortie = dateDeSortie;
+	}
 
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", dateDeSortie=" + dateDeSortie + "]";
 	}
 	
 	
--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -6,7 +6,8 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		Console c1 = new Console("Nintendo DS");
+		Console c1 = new Console("Nintendo DS", 50, "2005-10-15");
+		Adresse a1 = new Adresse(22, "rue du jeu", "Paris");
 		
 		Jeu jeu1 = new Jeu("New Super Mario Bros", c1);
 		Jeu jeu2 = new Jeu("The Legend Of Zelda", c1);
@@ -14,6 +15,7 @@ public class Test {
 		Jeu jeu4 = new Jeu("Pokemon Rubis", c1);
 		Jeu jeu5 = new Jeu("Sims City", c1);
 
+		Boutique b1 = new Boutique ("GameStore", a1 );
 
 		
 	}
